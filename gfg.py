ce5a2ecf25b43d40271a2fc53e2191a9430bc681
bhargavas <bhargavawrites@gmail.com>
2023-11-02 17:50:45 +0530

    commit

---
+++ gfg.py
@@ -0,0 +1,7 @@
+"""
+This is a module docstring.
+"""
+
+A = 1  # Updated variable name to conform to UPPER_CASE naming style
+B = 2  # Updated variable name to conform to UPPER_CASE naming style
+print(A + B)  # Added spaces around the addition operator for readability
